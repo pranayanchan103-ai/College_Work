5b05e7adaa1899bd6b8b4b5b04c68a28200ef4f7
Pranay Anchan <pranayanchan103@gmail.com>
2026-07-15 12:28:08 +0530

    Add files via upload

---
+++ AIDSA/T1.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() {
+		int n;
+		printf("\nEnter the number of elements you want inside an array:");
+		scanf("%d",&n);
+		int arr[n];
+		printf("Input the elements you want inside an array: ");
+			for(int i=0 ; i<n ; i++) {
+				scanf("%d",&arr[i]);
+			}
+            int count=0;
+            int target;
+            int temp=0;
+            printf("\n Enter your target element! ");
+            scanf("\n %d",&target);
+                for(int i=0 ; i<n ; i++){
+                    if(arr[i]==target) {
+                        count++;
+                            temp=i;
+                                if(temp>=0) {
+                                    break;
+                                }
+                }
+        }
+            if(count==0) {
+                printf("\n the element was not found in the array!");
+            }
+                else {
+                    printf("\n the number of times the target found was: %d",count);
+                printf("\n the first time it was found was on index: %d",temp);
+                }
+				return 0;
+	}
