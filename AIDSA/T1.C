87ac71e7a293fd444e9b9f92256913d60756868a
Pranay Anchan <pranayanchan103@gmail.com>
2026-07-22 12:28:00 +0530

    Update T1.C

--- AIDSA/T1.C
+++ AIDSA/T1.C
@@ -1,33 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main() {
-		int n;
-		printf("\nEnter the number of elements you want inside an array:");
-		scanf("%d",&n);
-		int arr[n];
-		printf("Input the elements you want inside an array: ");
-			for(int i=0 ; i<n ; i++) {
-				scanf("%d",&arr[i]);
-			}
-            int count=0;
-            int target;
-            int temp=-1;
-            printf("\n Enter your target element! ");
-            scanf("\n %d",&target);
-                for(int i=0 ; i<n ; i++){
-                    if(arr[i]==target) {
-                            temp=i;
-                                if(count==0) {
-                                    temp=i;
-                                }
-                                count++;
-                }
-        }
-            if(count==0) {
-                printf("\n the element was not found in the array!");
+    int n;
+
+    printf("Enter the number of elements you want inside an array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Input the elements you want inside an array: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int count = 0;
+    int target;
+    int temp = -1;
+
+    printf("Enter your target element: ");
+    scanf("%d", &target);
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) {
+            if(count == 0) {
+                temp = i;
             }
-                else {
-                    printf("\n the number of times the target found was: %d",count);
-                printf("\n the first time it was found was on index: %d",temp);
-                }
-				return 0;
-	}
+            count++;
+        }
+    }
+
+    if(count == 0) {
+        printf("The element was not found in the array!\n");
+    } else {
+        printf("The number of times the target was found: %d\n", count);
+        printf("The first occurrence was at index: %d\n", temp);
+    }
+
+    return 0;
+}
